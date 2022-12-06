5ffef269ecb6d1edf6f7c4951f63a08218dca627
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2022-12-06 10:08:27 +0000

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 5}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left+dodger.clientWidth < 400) {
+      dodger.style.left = `${left + 5}px`;
+    }
+  }
+
+  
+  const dodger = document.getElementById("dodger");
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+      }
+  });
